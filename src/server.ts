12439a06f20f605a9a7d880b2af535ef161e33d0
Renan Salustiano <renan.salustiano@ter.grupomarista.org.br>
2024-11-10 17:40:37 -0300

    add helmet

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
 import express from "express";
+import helmet from "helmet";
 import dotenv from 'dotenv';
 dotenv.config();
 
 import routes from './app/config/routes'
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
+app.use(helmet());
 app.use(express.json())
 app.use(routes)
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
 });
